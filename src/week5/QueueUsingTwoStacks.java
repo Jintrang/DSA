0a7d3aaa80464a78017ba37a1368c5974610244e
Jintrang <80148026+Jintrang@users.noreply.github.com>
2021-10-14 16:36:02 +0700

    week5 new

--- src/week5/QueueUsingTwoStacks.java
+++ src/week5/QueueUsingTwoStacks.java
@@ -11,50 +11,44 @@ import java.util.regex.*;
 
 
 public class QueueUsingTwoStacks {
-    public static class MyQueue {
-        int[] arr;
-        int size;
 
-        public MyQueue(int n) {
-            arr = new int[n];
-            size = 0;
+        public static void enqueue(Stack<Integer> sIn, int n) {
+            sIn.push(n);
         }
 
-        public void enqueue(int n) {
-            arr[size++] = n;
-        }
-
-        public int dequeue() {
-            if (size == 0) throw new NoSuchElementException("null");
-            int res = arr[0];
-            for (int i = 0; i < size - 1; i++) {
-                arr[i] = arr[i + 1];
+        public static int dequeue(Stack<Integer> sIn, Stack<Integer> sOut) {
+            if(sOut.empty()) {
+                while(!sIn.empty()){
+                    sOut.push(sIn.pop());
+                }
             }
-            arr[size - 1] = Integer.parseInt(null);
-            size--;
-            return res;
+            return sOut.pop();
         }
 
-        public void print() {
-            if(size == 0) throw new NoSuchElementException("null");
-            System.out.println(arr[0]);
+        public static void print(Stack<Integer> sIn, Stack<Integer> sOut) {
+            if(sOut.empty()) {
+                while(!sIn.empty()){
+                    sOut.push(sIn.pop());
+                }
+            }
+            System.out.println(sOut.peek());
         }
-    }
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        MyQueue abc = new MyQueue(n);
+        Stack<Integer> sIn = new Stack<>();
+        Stack<Integer> sOut = new Stack<>();
         for(int i = 0; i < n; i++) {
             int type = scan.nextInt();
             if (type == 1) {
                 int x = scan.nextInt();
-                abc.enqueue(x);
+                enqueue(sIn,x);
             } else if (type == 2) {
-                abc.dequeue();
+                dequeue(sIn, sOut);
             } else {
-                abc.print();
+                print(sIn, sOut);
             }
         }
     }
